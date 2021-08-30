8adabe7b1cb4a683c05054b06fe585099bdcc804
Adam Svensson <therealadamzki@gmail.com>
2021-08-30 15:49:37 +0200

    An example of what we can do. I have added some bells and whistles that can be elaborated upon or removed

---
+++ example.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import Adafruit_DHT as dht # Importing a library used for the DHT22 which is the same as our AM2302 
+from time import sleep
+from datetime import datetime
+
+# These might be placed in a seperate "constants.py"
+_ledPin = 0
+_am2302Pin = 0
+
+humThreashold = 0
+tempThreashold = 0
+
+def main():
+
+    #Generall GPIO setup
+    GPIO.setmode(GPIO.BCM) # Each pin on the Raspberry Pi has several different names, so you need to tell the program which naming convention is to be used.
+    GPIO.setwarnings(False)
+
+    GPIO.setup(_ledPin, GPIO.OUT) # Setting the led pin as an output to be able to turn it on and off
+    
+    while True:
+        
+        h,t = dht.read_retry(dht.DHT22, _am2302Pin) # Reading the temperature (t) and humidity (h)
+
+        if h > humThreashold:
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print("Warning: The humidity is high - ", dt_string)
+            GPIO.output(_ledPin, GPIO.HIGH) # Turning on the LED
+        else:
+            GPIO.output(_ledPin, GPIO.LOW) # Turning off the LED
+
+        # If the temperature is higher than a given threashold the led will flash
+        while t > tempThreashold:
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print("Warning: The temperature is high - ", dt_string)
+            GPIO.output(18,GPIO.HIGH)
+            sleep(0.5)
+            GPIO.output(18,GPIO.LOW)
+             
+if __name__ == "__main__":
+    main()
